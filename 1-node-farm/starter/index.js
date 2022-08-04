acb0f4280e876a803c36868b145710fa720b0608
ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2022-08-04 15:38:09 +0500

    adding some routes to application

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,20 +1,22 @@
 const fs = require('fs');
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+const http = require('http');
+const url = require('url');
 
-// const textOut = `this is what we now about avokado:  ${textIn} \n Created on ${Date.now()}`;
-
-// fs.writeFileSync('./txt/output.txt', textOut);
-
-// asynchronus way
-
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-      console.log(data3);
-      fs.writeFile('./txt/final.txt', `${data2}\n ${data3}`, 'utf-8', (err) => {
-        console.log('your file has been written!');
-      });
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('this is a overview!');
+  } else if (pathName === '/product') {
+    res.end('this is a product!');
+  } else {
+    res.writeHead(404, {
+      'Content-Type': 'text/html',
+      'my-own-header': 'hello header',
     });
-  });
+    res.end('<h1>page not found</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('listening 8000');
 });

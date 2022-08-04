4e9b67fe594e9053d731011b243f32b48654203f
ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2022-08-04 21:11:47 +0500

    added some data to backend

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,12 +2,20 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   if (pathName === '/' || pathName === '/overview') {
     res.end('this is a overview!');
   } else if (pathName === '/product') {
     res.end('this is a product!');
+  } else if (pathName === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+    res.end(data);
   } else {
     res.writeHead(404, {
       'Content-Type': 'text/html',

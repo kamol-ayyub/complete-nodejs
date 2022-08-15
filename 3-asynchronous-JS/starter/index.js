685d5d671e8104dbff09fc1bac539a2ee287578b
ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2022-08-15 15:49:37 +0500

    some changes

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,16 +1,29 @@
 const fs = require('fs');
 const superagent = require('superagent');
+const readFilePro = (file) => {
+  return new Promise((res, rej) => {
+    if (err) reject(' i couldnt not find');
+    fs.readFile(file),
+      (err, data) => {
+        res(data);
+      };
+  });
+};
+//
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(data, 'data');
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
+    .then((res) => {
       console.log(res.body.message);
       if (err) return console.log(err.message, 'error');
 
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
         console.log('random dog img file saved');
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });

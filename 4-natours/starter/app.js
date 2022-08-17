cd0b1c48123f7630c85a7734e700c9bf2aaab446
ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2022-08-17 18:56:48 +0500

    req.params added

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -19,6 +19,17 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
 
 app.post('/api/v1/tours', function (req, res) {
   const newID = tours[tours.length - 1].id + 1;

875776dcd8619a2da3f5bd25bea48f0bc6416d3b
ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2022-08-17 11:53:29 +0500

    we did get requests on expressjs

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,12 +1,19 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) =>
-  res.status(200).json({ message: 'Hello World!', app: 'Natours' })
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-
-app.post('/', (req, res) => {
-  res.send('POST request to the homepage');
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

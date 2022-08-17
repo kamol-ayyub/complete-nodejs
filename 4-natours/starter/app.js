726cc60eee60bbb6a5b1cd05847595152cbaed71
ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2022-08-17 17:34:58 +0500

    get api succesfully added

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+
 const app = express();
+app.use(express.json());
 const port = 3000;
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -16,4 +20,22 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.post('/api/v1/tours', function (req, res) {
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+  console.log(req.body);
+});

e4321834dd25841831032b519f0105b38f9f61e5
ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2022-08-20 16:57:42 +0500

    dotenv file added

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -5,7 +5,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //  middlewares
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,3 +1,7 @@
 const app = require('./app');
+const dotenv = require('dotenv');
 const port = 3000;
+dotenv.config({
+  path: './config.env',
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

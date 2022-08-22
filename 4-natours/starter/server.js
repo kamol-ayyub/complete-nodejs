3c5c466f53b8c245cd6ea59ace9279c57a933ad2
ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2022-08-22 15:33:15 +0500

    final changes on winodwS

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,26 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const port = process.env.PORT || 3000;
+const app = require('./app');
 dotenv.config({
   path: './config.env',
 });
-const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  `<PASSWORD>`,
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log('DB connecion successfull');
+  });
+
+const tourSchema = new mongoose.Schema({});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
